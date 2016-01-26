e66e82be1408ca18a4636267af86dd6e5af252b7
crizlucero <crizlucero@gmail.com>
2016-01-26 13:28:54 -0600

    Se guardan los correos y telefonos en listas sin repetición.

--- SpamLord.py
+++ SpamLord.py
@@ -3,7 +3,9 @@ from os import listdir
 from os.path import isfile, join
 
 onlyfiles = [f for f in listdir("MIT Sloan Staff Directory") if isfile(join("MIT Sloan Staff Directory",f))]
-
+count1, count2 = 0,0
+correos = []
+telefonos = []
 #abrir Archivo
 for files in onlyfiles:
     c = open("Correos.txt","r")
@@ -15,7 +17,11 @@ for files in onlyfiles:
             line = line.strip('\n')
             m = re.findall(line,row)
             if len(m) > 0:
-                print m
+                for mail in m:
+                    if(not mail in correos):
+                        correos.append(mail)
+                #print m[0]
+                count1 = count1 + 1
         line = None
     c.close()
     h.seek(0)
@@ -24,9 +30,16 @@ for files in onlyfiles:
             line = line.strip('\n')
             m = re.findall(line,row)
             if len(m) > 0:
-                print m
+                for tel in m:
+                    if(not tel in telefonos):
+                        telefonos.append(tel)
+                #print m[0]
+                count2 = count2 + 1
         line = None
     t.close()
-
+print len(correos)
+print len(telefonos)
+print count1
+print count2
 
 
